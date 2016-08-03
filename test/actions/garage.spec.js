47df033b3eacd6d27439198b25e7183bba411afe
Callum Smits <callum.smits@gmail.com>
2016-08-03 14:42:37 +1000

    Created test for measuredDistance action
     Changes to be committed:
    	new file:   test/.eslintrc.json
    	new file:   test/actions/garage.spec.js

---
+++ test/actions/garage.spec.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import * as actions from '../../actions';
+
+const expect = chai.expect;
+
+const error = new TypeError('not a number');
+describe('garage actions', function () {
+  it('measuredDistance should create MEASURED_DISTANCE action', function() {
+    expect(actions.measuredDistance(5)).to.deep.equal({
+      type: 'MEASURED_DISTANCE',
+      payload: 5,
+    });
+  });
+  it('measuredDistance should create MEASURED_DISTANCE action with error', function () {
+    expect(actions.measuredDistance(error)).to.deep.equal({
+      type: 'MEASURED_DISTANCE',
+      payload: error,
+      error: true,
+    });
+  });
+});
