1e96266f5b556a6e66fbd62134c46093df952155
Callum Smits <callum.smits@gmail.com>
2016-08-03 23:18:59 +1000

    Refactored reducers into separate files
     Changes to be committed:
    	modified:   reducers/index.js

--- reducers/index.js
+++ reducers/index.js
@@ -1,28 +1,10 @@
 import { combineReducers } from 'redux';
+import door from './door.js';
+import secure from './secure.js';
 
-const garageReducer = (state = { secure: 'OFF', door: 'CLOSED' }, action) => {
-  switch (action.type) {
-    case 'UNSECURE_DOOR':
-      if (state.secure === 'OFF') {
-        return Object.assign({}, state, {
-          secure: 'TURNING_ON',
-        });
-      }
-      return state;
-    case 'TURN_ON_TIMEOUT':
-      if (state.secure === 'TURNING_ON') {
-        return Object.assign({}, state, {
-          secure: 'ON',
-        });
-      }
-      return state;
-    case 'SECURE_DOOR':
-      return Object.assign({}, state, {
-        secure: 'OFF',
-      });
-    default:
-      return state;
-  }
-};
+const garageReducer = combineReducers({
+  secure,
+  door,
+});
 
 export default garageReducer;
