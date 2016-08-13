383c6c0079cd1606c139ba91a910957c1378da01
Callum Smits <callum.smits@gmail.com>
2016-08-14 01:00:48 +1000

    Add secure state to state display
     Changes to be committed:
    	modified:   components/stateDisplay-test.jsx
    	modified:   components/stateDisplay.jsx

--- components/stateDisplay-test.jsx
+++ components/stateDisplay-test.jsx
@@ -26,4 +26,16 @@ describe('<StateDisplay />', function () {
     expect(wrapperUnknown.find('h2').text()).to.equal('Unknown');
   });
 
+  it('should render secure states appropriately', function () {
+    const wrapper = shallow(<StateDisplay doorState="OPEN" secureState="OFF" />);
+    expect(wrapper.find('h3').text()).to.equal('Secure');
+    const wrapperOn = shallow(<StateDisplay doorState="OPEN" secureState="ON" />);
+    expect(wrapperOn.find('h3').text()).to.equal('Unlocked');
+    const wrapperTurnOn = shallow(<StateDisplay doorState="OPEN" secureState="TURN_ON_REQUEST" />);
+    expect(wrapperTurnOn.find('h3').text()).to.equal('Unlocking');
+    const wrapperTurnOnComplete = shallow(<StateDisplay doorState="OPEN" secureState={{ state: 'TURNING_ON', id: 143 }} />);
+    expect(wrapperTurnOnComplete.find('h3').text()).to.equal('Unlocking');
+    const wrapperTurnOff = shallow(<StateDisplay doorState="OPEN" secureState="TURN_OFF_REQUEST" />);
+    expect(wrapperTurnOff.find('h3').text()).to.equal('Locking');
+  });
 });
--- components/stateDisplay.jsx
+++ components/stateDisplay.jsx
@@ -19,8 +19,30 @@ const StateDisplay = ({ doorState, secureState }) => {
       stateText = 'Unknown';
   }
 
+  let secureText = '';
+  if (secureState.state) {
+    secureText = 'Unlocking';
+  } else {
+    switch (secureState) {
+      case 'ON':
+        secureText = 'Unlocked';
+        break;
+      case 'OFF':
+        secureText = 'Secure';
+        break;
+      case 'TURN_ON_REQUEST':
+        secureText = 'Unlocking';
+        break;
+      case 'TURN_OFF_REQUEST':
+        secureText = 'Locking';
+        break;
+      default:
+    }
+  }
+
   return (
     <div>
+      <h3>{secureText}</h3>
       <h2>{stateText}</h2>
     </div>
   );
@@ -28,7 +50,10 @@ const StateDisplay = ({ doorState, secureState }) => {
 
 StateDisplay.propTypes = {
   doorState: PropTypes.string.isRequired,
-  secureState: PropTypes.string.isRequired,
+  secureState: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+  ]),
 };
 
 export default StateDisplay;
